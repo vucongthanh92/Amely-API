d3b42f3bf7160f457efc487ca2c2c9c7356a2910
Thinh <cocungkhongcho@gmail.com>
2018-11-20 01:29:07 +0700

    turn off error

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors', 0);
+error_reporting(E_ERROR | E_WARNING | E_PARSE); 
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
