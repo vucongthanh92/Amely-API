38b7b965776b15227b52c305a6db62df91f361f9
Nguyen Thinh <thinhn@dfm-engineering.com>
2018-11-30 17:49:02 +0700

    vouchers post

---
+++ routes/vouchers.php
@@ -0,0 +1,37 @@
+<?php
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+$app->post($container['prefix'].'/vouchers', function (Request $request, Response $response, array $args) {
+	$productService = ProductService::getInstance();
+	$params = $request->getParsedBody();
+	if (!$params) $params = [];
+	if (!array_key_exists('offset', $params))  	$params['offset'] = 0;
+	if (!array_key_exists('limit', $params))  	$params['limit'] = 10;
+
+	$product_params = null;
+	$product_params[] = [
+		'key' => "is_special",
+		'value' => "= 1",
+		'operation' => ""
+	];
+	$product_params[] = [
+		'key' => "approved",
+		'value' => "> 0",
+		'operation' => "AND"
+	];
+	$product_params[] = [
+		'key' => "status",
+		'value' => "= 1",
+		'operation' => "AND"
+	];
+	$product_params[] = [
+		'key' => "time_created",
+		'value' => "DESC",
+		'operation' => "order_by"
+	];
+
+	$products = $productService->getProducts($product_params, $offset, $limit);
+	
+	return response($products);
+});
