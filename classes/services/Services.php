47f2c858c09efa44b7f7228e9ffe94bd472c902a
Nguyen Thinh <thinhn@dfm-engineering.com>
2018-12-04 14:41:52 +0700

    fix bug

--- classes/services/Services.php
+++ classes/services/Services.php
@@ -25,14 +25,14 @@ class Services extends SlimDatabase
     		$obj = $result[0];
     		$properties = get_object_vars($obj);
     		foreach ($properties as $key => $value) {
-    			$obj->$key = html_entity_decode($value);
+    			$obj->$key = htmlspecialchars_decode($value);
     		}
     		return $obj;
     	}
     	foreach ($result as $k => $obj) {
     		$properties = get_object_vars($obj);
     		foreach ($properties as $key => $value) {
-    			$obj->$key = html_entity_decode($value);
+    			$obj->$key = htmlspecialchars_decode($value);
     		}
     		$result[$k] = $obj;
     	}
