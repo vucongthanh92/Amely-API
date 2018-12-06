b0b5507a20e6e51d6011ff0618aca395576cf7da
Nguyen Thinh <thinhn@dfm-engineering.com>
2018-12-06 10:26:26 +0700

    fix bug

--- src/middleware.php
+++ src/middleware.php
@@ -14,19 +14,19 @@ class AuthMiddleware
     	$user_token_ignore = ["appupdate", "register", "authtoken", "activation", "resize", "download_file", "forgot_password", "services", "payment_response"];
 
     	$route = $request->getAttribute('route');
-        if (!$route) return response(false);
+        if (!$route) return responseError("token_error");
         $name = $route->getName();
         if (in_array($name, $user_token_ignore)) {
             return $next($request, $response);
         } else {
 		    $params = $request->getQueryParams();
-            if (!array_key_exists("user_token", $params)) return response(false);
+            if (!array_key_exists("user_token", $params)) return responseError("token_error");
             $tokenService = TokenService::getInstance();
 
             if ($tokenService->checkToken($params['user_token'])) {
     			return $next($request, $response);
     		}
-    		return response(false);
+    		return responseError("token_error");
     	}
     }
 }
