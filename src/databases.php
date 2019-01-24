0df2d5db9c0e375c99dbddfd70de6d692d132a1e
thinhn <cocungkhongcho@gmail.com>
2019-01-24 16:10:47 +0700

    fix bug

--- src/databases.php
+++ src/databases.php
@@ -295,6 +295,7 @@ class SlimDatabase
 			} else {
 				array_push($params['values'], htmlspecialchars($value, ENT_QUOTES, 'UTF-8'));
 			}
+			$value = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 			$params['sets'][] = "`{$key}` = '{$value}'";
 			if ($key == 'id') {
 				$id_response = $value;
